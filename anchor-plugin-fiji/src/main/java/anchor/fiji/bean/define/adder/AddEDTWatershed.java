dc0444c0aef6f03c6ee90aefc899990099c2c68d
Owen Feehan <owenfeehan@users.noreply.github.com>
2020-05-30 19:45:47 +0200

    Including error filename source when DefineAdders are used

--- anchor-plugin-fiji/src/main/java/anchor/fiji/bean/define/adder/AddEDTWatershed.java
+++ anchor-plugin-fiji/src/main/java/anchor/fiji/bean/define/adder/AddEDTWatershed.java
@@ -275,5 +275,4 @@ public class AddEDTWatershed extends DefineAdderWithPrefixBean {
 	public void setMaxDistanceDeltaContour(double maxDistanceDeltaContour) {
 		this.maxDistanceDeltaContour = maxDistanceDeltaContour;
 	}
-
 }
